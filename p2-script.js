7f61ab829e544a5656fdd30e45424df28c5c6961
louismnt <louis.manotte@gmail.com>
2023-11-25 11:16:39 +0100

    Init

---
+++ p2-script.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById("btn");
+    const videos = document.querySelectorAll(".video-opacite");
+    const videoOutro = document.getElementById("video-outro");
+    const videoIntro = document.querySelector(".intro");
+
+    btn.addEventListener('touchstart', function (event) {
+        event.preventDefault();
+        videoIntro.classList.add('flou');
+        const timerId = setTimeout(lancerVideoAleatoire, 2000);
+        btn.dataset.timerId = timerId;
+    });
+
+    btn.addEventListener('touchend', function () {
+        clearTimeout(btn.dataset.timerId);
+        videoIntro.classList.remove('flou');
+    });
+
+
+
+    function testVideoInitial() {
+        for(videoAleatoire = 0;videoAleatoire = 5; videoAleatoire = videoAleatoire + 1){
+        videoIntro.pause();
+        videoAleatoire.play();
+        videoIntro.play();
+    }
+    }
+
+
+
+    function lancerVideoAleatoire() {
+        stopperToutesLesVideos();
+        
+
+        const videoAleatoire = videos[Math.floor(Math.random() * videos.length)];
+        videoAleatoire.style.opacity = '1';
+        videoAleatoire.play();
+        videoIntro.pause();
+        videoAleatoire.addEventListener('ended', function () {
+            videoBonus();
+        });
+    }
+
+    function stopperToutesLesVideos() {
+        videos.forEach(video => {
+            video.pause();
+            video.currentTime = 0;
+            video.style.opacity = '0';
+        });
+    }
+
+    function videoBonus() {
+        stopperToutesLesVideos();
+
+        videoOutro.style.opacity = '1';
+        videoOutro.play();
+
+        videoOutro.addEventListener('ended', function () {
+            videoOutro.style.opacity = '0';
+            videoIntro.currentTime = 0;
+            videoIntro.play();
+        });
+    }
+});
